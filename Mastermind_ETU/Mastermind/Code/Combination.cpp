d336d552156d40b60d8e78588f2afbb47e0d3e3b
William Crépault <william.crepault@gmail.com>
2024-09-22 17:59:48 -0400

    Complétion des surcherges < et > dans Combinaison.cpp pour mettre la liste dans le bon ordre de combinaison numérique (suivant les valeurs numérique des couleurs)

--- Mastermind_ETU/Mastermind/Code/Combination.cpp
+++ Mastermind_ETU/Mastermind/Code/Combination.cpp
@@ -20,14 +20,46 @@ Combination::~Combination()
 {
 }
 
-bool Combination::operator< (const Combination& _combination) const
+bool Combination::operator < (const Combination& _combination) const
 {
-	return true;
+	int left = 0;
+	int multiplier = 1000;
+	for (short color = 0; color < COMBINATION_LENGTH; color++)
+	{
+		left += tabColors[color].convertToNumeric() * multiplier;
+		multiplier /= 10;
+	}
+
+	int right = 0;
+	multiplier = 1000;
+	for (short color = 0; color < COMBINATION_LENGTH; color++)
+	{
+		right += _combination.tabColors[color].convertToNumeric() * multiplier;
+		multiplier /= 10;
+	}
+
+	return left < right;
 }
 
 bool Combination::operator > (const Combination& _combination) const
 {
-	return true;
+	int left = 0;
+	int multiplier = 1000;
+	for (short color = 0; color < COMBINATION_LENGTH; color++)
+	{
+		left += tabColors[color].convertToNumeric() * multiplier;
+		multiplier /= 10;
+	}
+
+	int right = 0;
+	multiplier = 1000;
+	for (short color = 0; color < COMBINATION_LENGTH; color++)
+	{
+		right += _combination.tabColors[color].convertToNumeric() * multiplier;
+		multiplier /= 10;
+	}
+
+	return left > right;
 }
 
 bool Combination::operator == (const Combination& _combination) const
@@ -49,11 +81,11 @@ bool Combination::operator != (const Combination& _combination) const
 
 void Combination::display() const
 {
-	cout << "[" << tabColors[0] << ", " 
-		 << tabColors[1] << ", " 
-		 << tabColors[2] << ", " 
-		 << tabColors[3] << "]" << endl;
-    /*cout << tabColors[0] << endl;
+	cout << "[" << tabColors[0] << ", "
+		<< tabColors[1] << ", "
+		<< tabColors[2] << ", "
+		<< tabColors[3] << "]" << endl;
+	/*cout << tabColors[0] << endl;
 	cout << tabColors[1] << endl;
 	cout << tabColors[2] << endl;
 	cout << tabColors[3] << "]" << endl;*/
