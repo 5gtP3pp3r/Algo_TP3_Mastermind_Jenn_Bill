228487621566e911f0ee5013ad03950cde56cab4
2394947 <jennifer.m1999@outlook.com>
2024-09-25 14:49:18 -0400

    Modifications et corrections syntaxes des commentaires de mastermind.cpp

--- Mastermind_ETU/Mastermind/Code/Mastermind.cpp
+++ Mastermind_ETU/Mastermind/Code/Mastermind.cpp
@@ -19,11 +19,11 @@ Mastermind::~Mastermind()
 	for (short node = 0; node < LISTLENGTH; node++)
 	{
 		delete tabCombinations[node];									// Nous conservons les adresses des combinaisons dans un tableau afin de
-	}																	// conserver une "poigné" dessus et pouvoir les détruire à la fin du programmes.
+	}																	// conserver une "poigné" dessus et pouvoir les détruires à la fin du programmes.
 }
 
 /// <summary>
-/// Retourne le nombre d'éléments dans la liste chainées.
+/// Retourne le nombre d'éléments dans la liste chainée.
 /// </summary>
 /// <returns> Le nombre d'éléments. </returns>
 int Mastermind::getNbElements() const
@@ -52,7 +52,7 @@ Combination* Mastermind::getElement(int _index) const
 
 /// <summary>
 /// Logique de comparaison de la combinaison choisi par le cpu avec chaque combinaison de la liste
-/// en relation avec les qualités de chaque couleur des combinaison du tableau _tabVerdicts
+/// en relation avec les qualités de chaque couleur des combinaisons du tableau _tabVerdicts
 /// </summary>
 /// <param name="_toValidate"> Combinaison de la liste à valider. </param>
 /// <param name="_tried"> Combinaison aléatoire à comparer avec chaque combinaisons de la liste à valider. </param>
@@ -60,7 +60,7 @@ Combination* Mastermind::getElement(int _index) const
 /// <returns> Retour booléen sur la validité des combinaisons de la liste. </returns>
 bool Mastermind::isPossibleCombination(Combination* _toValidate, Combination* _tried, short* _tabVerdicts) const
 {
-	Color colorTried = NULL;											// Déclarations de type couleur pour la combinaison 
+	Color colorTried = NULL;											// Déclaration de type couleur pour la combinaison 
 	Color colorValidate = NULL;											// _tried et chaque combinaisons de la liste.
 	bool isCondition3 = true;
 
@@ -69,45 +69,45 @@ bool Mastermind::isPossibleCombination(Combination* _toValidate, Combination* _t
 		colorTried = _tried->getColor(value);							// Attribution des valeurs des couleurs en relation 
 		colorValidate = _toValidate->getColor(value);					// avec les itérations du tableau _tabVerdicts.
 
-		if (_tabVerdicts[value] == 1 && colorTried != colorValidate)	// Si la qualité est 1, bonne couleur, bonne position, mais que la couleur ne
-		{																// concordent pas, la combinaison de la liste sera false. Donc retirée dans cleanList().
+		if (_tabVerdicts[value] == 1 && colorTried != colorValidate)	// Si la qualité est 1 (bonne couleur, bonne position) et les couleurs sont
+		{																// différentes, la combinaison de la liste est invalide. Donc retirée dans cleanList().
 			return false;
 		}
-		if (_tabVerdicts[value] == 2 && colorTried == colorValidate)	// Si la qualité est 2, bonne couleur, mauvaise position, mais que la couleur est 
-		{																// la même, la combinaison de la liste sera false. Donc retirée dans cleanList().
+		if (_tabVerdicts[value] == 2 && colorTried == colorValidate)	// Si la qualité est 2 (bonne couleur, mauvaise position) et les couleurs sont 
+		{																// identiques (au meme index), la combinaison de la liste est invalide. Donc retirée dans cleanList().
 			return false;
 		}
-		if (_tabVerdicts[value] == 2 && colorTried != colorValidate)	// Si la qualité est 2, bonne couleur, mauvaise 
-		{																// position, mais que la couleur n'est la même,
+		if (_tabVerdicts[value] == 2 && colorTried != colorValidate)	// Si la qualité est 2 (bonne couleur, mauvaise position), 
+		{																// et les couleurs sont différentes (au meme index),
 			if (!containsColor(_toValidate, colorTried, value))			// appel de la méthode containsColor, si la couleur
-			{															// n'apparait pas à un autre index, la combinaison de
-				return false;											// la liste sera false. Donc retirée dans cleanList().
+			{															// n'est pas à un autre index, la combinaison de
+				return false;											// la liste est invalide. Donc retirée dans cleanList().
 			}
 		}		
-		if (_tabVerdicts[value] == 3)									// Si la qualité est 3, cette couleur n'est pas dans la combinaison,
+		if (_tabVerdicts[value] == 3)									// Si la qualité est 3 (mauvaise couleur),
 		{
 			if (containsColor(_toValidate, colorTried, value))			// appel de la méthode containsColor, si la couleur
-			{															// apparait pas à un autre index, la combinaison de
-				return false;											// la liste sera false. Donc retirée dans cleanList().
+			{															// est trouvée, la combinaison est invalide. 
+				return false;											// Donc retirée dans cleanList().
 			}
 		}
 	}
 	return true;														// Si aucune condition ne s'applique, la combinaison
-}																		//  est bonne, donc ignoré dans la méthode cleanList().
+}																		// est bonne et restera dans la liste.
 
 /// <summary>
-/// Logique de retrait des combinaisons de la liste chainée. Itère la liste et envois à la méthode
-/// isPossibleCombinaison la combinaison présente de la liste, la combinaison essayé par le cpu et 
-/// la qualité entré par l'utilisateur. retirera de la liste une combinaison qui aura une valeur false.
-/// À chaque suppression, une va;eur est incrémenté pour retourner le nombre de combinaisons retirées.
+/// Logique de retrait des combinaisons de la liste chainée. Itère la liste et envoi à la méthode
+/// isPossibleCombinaison la combinaison présente de la liste, la combinaison essayée par le cpu et 
+/// la qualité entrée par l'utilisateur. Retirera de la liste une combinaison invalide.
+/// À chaque suppression, une valeur est incrémentée pour retourner le nombre de combinaisons retirées.
 /// </summary>
-/// <param name="_ref"> Combinaison choisi par le cpu. </param>
-/// <param name="_tabVerdicts"> qualité de la combinaison entré par l'utilisateur. </param>
-/// <returns> nombre de combinaisons retirées. </returns>
+/// <param name="_ref"> Combinaison choisie par le cpu. </param>
+/// <param name="_tabVerdicts"> Qualité de la combinaison entrée par l'utilisateur. </param>
+/// <returns> Nombre de combinaisons retirées. </returns>
 int Mastermind::cleanList(Combination* _ref, short* _tabVerdicts)
 {
-	short removedCombinations = 0;										// Déclaration d'une variable qui sera incrémenté au besoin.
-	short listLength = this->getNbElements();							// Déclaration d'une variable qui va conserver la longueur de la liste.
+	short removedCombinations = 0;										// Déclaration d'une variable pour retourner le nombre de combinaison retirées.
+	short listLength = this->getNbElements();							// Déclaration d'une variable qui contient la longueur de la liste.
 	Combination* combToValidate = NULL;									// Déclaration d'un type pointeur de Combination.
 
 	for (short node = 0; node < listLength; node++)						// Itération à la longueur de la liste
@@ -115,24 +115,29 @@ int Mastermind::cleanList(Combination* _ref, short* _tabVerdicts)
 		combToValidate = this->getElement(node);
 		if (!isPossibleCombination(combToValidate, _ref, _tabVerdicts))	// Appel de la méthode isPossibleCombination avec les paramètres pertinents.
 		{
-			list->remove(combToValidate);								// Si la condition est fausse, on retire la combinaison de la liste,
-			listLength--;												// décrémentation de la longueur de la boucle,
-			removedCombinations++;										// incrémentation de la variable représentant le nombre de combinaisons retirées
-			node--;														// et, particulier, décrémentation de l'index de la boucle.
-		}
+			list->remove(combToValidate);								// Si la combinaison est invalide, on retire la combinaison de la liste,
+			listLength--;												// décrémente de 1 la longueur de la boucle puisqu'on a retirée une combinaison,
+			removedCombinations++;										// incrémente de 1 le nombre de combinaisons retirées
+			node--;														// et, particulier, décrémente de 1 l'index de la boucle
+		}                                                               // pour se repositionner et donc éviter de passer au dessus d'un noeud.
 	}
 
 	return removedCombinations;											// Retour du nombre de combinaisons retirées.
 
 
-   /****************************************************************************************************************************************************
-	Explications de notre logique pour la méthode cleanList(). Notre premier jet impliquait une boucle while et la déclaration de 2 variable de type 
-	Iterator<Combination>. nous utilisions dons les 2 itérateur pour pour pouvoir conserver la valeur de l'itérateur même si on retire sa valeur de la 
-	list. Nous avons observé le code et remarqué que la méthode getElement() itère déjà dans la liste et que getNbElement() nous donne aussi la longueur
-	de la liste. nous les avons donc mis à profit. Une mécanique particulière c'est imposé et nous avons observé une situations où certaine combinaisons 
-	étaient ignorées lors des itérations. Nous avons donc découpé, débogué et inspecter chaque itération pour voir un phénomène de saut involontaire de
-	combinaison. Nous avons donc réglé le problème avec une décrémentation de l'index de la boucle (node) seulement si une combinaison est retiré de la
-	liste. Voici donc une représentation graphique de la raison de l'utilisation de la décrémentation:
+   /******************************************* Explication de notre logique pour la méthode cleanList() ***************************************************************
+	
+	Notre premier jet impliquait une boucle while et la déclaration de 2 variables de type Iterator<Combination>. 
+	Nous utilisions un iterateur "b" pour conserver la valeur de l'itérateur "a" puisqu'on retirait la valeur de la 
+	liste. (l'iterateur "a" se retrouvait à pointer sur du "junk")
+	
+	Nous avons observé le code et remarqué que la méthode getElement() itère déjà dans la liste et que getNbElement() nous donne aussi la longueur
+	de la liste. Nous les avons donc mis à profit. De plus, une mécanique particulière s'est imposée : nous avons observé une situation où certaines combinaisons 
+	étaient ignorées lors des itérations. Cette situation nous a ammené à découpé, débogué et inspecté chaque itération pour constater un phénomène de sauts involontaires de
+	combinaisons. 
+	Finalement, nous avons réglé le problème avec une décrémentation de l'index de la boucle (node) (si une combinaison est retirée de la liste). 
+	
+	Voici une représentation graphique qui explique l'utilisation de la décrémentation:
 	
 	Voici une liste de 7 index:						0   1   2   3   4   5   6
 
@@ -140,7 +145,7 @@ int Mastermind::cleanList(Combination* _ref, short* _tabVerdicts)
 	a été raccourcie.
 													0   1   2   3   4   5 
 
-	Mais une situation spécial s'observe:		    index retiré, valeur 3
+	Mais une situation spéciale s'observe:		    index retiré, valeur 3
 													0   1   2   *   4   5
 													liste raccourcie de 1
 													0   1   2  <-3(4) <-4(5)
@@ -149,16 +154,16 @@ int Mastermind::cleanList(Combination* _ref, short* _tabVerdicts)
 								    le nouvel index 3, précédemment 4, a donc été ignoré.
 				      décrémentation de l'index (node--) pour se remettre à la bonne position dans la liste.
 
-	La boucle se déplace donc "Back & Forth" lorsqu'il y a suppression et itère normalement lorsqu'il n'y a pas de suppression. Nous avons choisis cette 
-	logique car elle réutilise des ressources existantes et n'avons pas à déclaré 2 autres itérateurs pour une mécanique d'échange de valeurs suite à une 
-	suppression. La méthode est donc environ 3 fois plus courte, très concise et verbeuse. Elle peut être lue et comprise comme un texte.
+	La boucle se déplace en "Back & Forth" lors d'une suppression et itère normalement lorsqu'il n'y a pas de suppressions. Nous avons choisi cette 
+	logique car elle réutilise des méthodes existantes et cela évite de déclaré 2 itérateurs supplémentaires. 
+	La méthode est donc optimisée, très concise et verbeuse. Elle peut être lue et comprise comme un texte ;-).
     ****************************************************************************************************************************************************/
 }
 
 /// <summary>
-/// Génération de la liste chainées de combinaisons
+/// Génération de la liste chainée de combinaisons
 /// </summary>
-/// <param name="_list"> Liste déclaré dans ConsoleMenu mais vide, seulement instanciée. </param>
+/// <param name="_list"> Liste déclarée dans ConsoleMenu mais vide, seulement instanciée. </param>
 void Mastermind::generateList(LinkedList<Combination>* _list)
 {
 	for (short c1 = 1; c1 < NB_COLORS + 1; c1++)						// Puissance "3". Une boucle par "puissance" de la combinaison.  
@@ -177,10 +182,10 @@ void Mastermind::generateList(LinkedList<Combination>* _list)
 }
 
 /// <summary>
-/// Ajout des combinaison de la liste dans le tableau prévue pour conserver
-/// les adresse pour permettre leur suppression dans le destructeur.
+/// Ajout des combinaison de la liste dans le tableau prévu pour conserver
+/// les adresses permettant leur suppression dans le destructeur.
 /// </summary>
-/// <param name="_list"> Liste nouvellement créé. </param>
+/// <param name="_list"> Liste nouvellement créée. </param>
 void Mastermind::fillTab(LinkedList<Combination>* _list)
 {
 	Node<Combination>* currentNode = _list->getFirstNode();
